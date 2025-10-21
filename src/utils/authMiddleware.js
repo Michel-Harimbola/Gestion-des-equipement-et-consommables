49da0e0f5ba53e0e9791adffcd856b4def0bf0bf
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-21 21:47:03 +0300

    feat: add logic authMeddleware

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -2,3 +2,36 @@ const jwt = require('jsonwebtoken');
 const prisma = require('../lib/prisma');
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
+async function protect(req, res, next) {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Extraire le jeton (ex: "Bearer <token>")
+            token = req.headers.authorization.split(' ')[1];
+
+            // Verifier le jeton et obtenir l'ID de l'utilisateur
+            const decoded = jwt.verify(token, JWT_SECRET);
+
+            // Charger l'useur dans l'objet req (sans le mdp)
+            req.user = await prisma.Utilisateur.findUnique({
+                where: { id: decoded.userId },
+                select: { id: true, email: true, nom: true, prenom: true, role: true }
+            });
+
+            if (!req.Utilisateur) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé, jeton invalide.' });
+            }
+
+            // Continuer vers le contrôleur
+            next();
+        } catch (error) {
+            res.status(401).json({ error: "Non autorisé, jeton invalide ou expiré." });
+        }
+    } else {
+        res.status(401).json({ error: "Non autorisé, aucun jeton fourni." });
+    }
+}
+
+module.exports = protect;

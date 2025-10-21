5047f922c9685e784d3f1cce9c795a180b214dd3
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-21 22:30:26 +0300

    feat: auth config in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const authRouter = require('./routes/auth.route');
+
 const app = express();
+
 app.use(express.json());
 
+app.use('/api/auth', authRouter);
 
+// Route de base(pour le test)
+app.get('/', (req, res) => {
+    res.send("API is running.");
+});
 
 module.exports = app;

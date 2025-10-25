4e7dec3629b3b30473ab7fd126aabeb3598af79c
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-26 00:14:21 +0300

    feat: crud user done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const authRouter = require('./routes/auth.route');
+const authRouter = require('./routes/auth.route.js');
+const userRouter = require('./routes/user.route.js');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 // Route de base(pour le test)
 app.get('/', (req, res) => {

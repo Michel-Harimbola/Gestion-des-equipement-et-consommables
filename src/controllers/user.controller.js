f9df92d513c4130c863c3ecb94e150d480e18338
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-25 23:46:09 +0300

    feat: user.controller.js

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,47 @@
+const UserService = require("../services/user.service.js");
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = await UserService.createUser(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await UserService.getUserById(req.params.id);
+        if(!user) throw new Error("Utilisateur non trouvé");
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+exports.getAllusers = async (req, res) => {
+    try {
+        const users = await UserService.getAllUsers();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await UserService.updateUser(req.params.id, req,body);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await UserService.deleteUser(req.params.id);
+        res.status(204).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}

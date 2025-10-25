c1f7360994d5394f96ba8c8c8f8f40fb4ca7726d
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-25 22:22:09 +0300

    feat: authentification done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,27 +1,27 @@
-const AuthServie = require('../services/auth.service.js');
+const AuthService = require('../services/auth.service.js');
 
 exports.register = async (req, res) => {
     try {
-        const result = await AuthServie.register(req.body);
+        const result = await AuthService.register(req.body);
         res.status(201).json(result);
     } catch (err) {
-        res.status(400).json({ error: err.mesage });
+        res.status(400).json({ error: err.message });
     }
 }
 
 exports.login = async (req, res) => {
     try {
-        const result = await AuthServie.login(req.body);
+        const result = await AuthService.login(req.body);
         res.status(200).json(result);
     } catch (err) {
-        res.status(400).json({ error: err.mesage });
+        res.status(400).json({ error: err.message });
     }
 }
 
 exports.changePassword = async (req, res) => {
     try {
         const { oldPassword, newPassword } = req.body;
-        const result = await AuthServie.changePassword(
+        const result = await AuthService.changePassword(
             req.params.id,
             oldPassword,
             newPassword,
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,81 +1,82 @@
-const prisma = require("../lib/prisma.js");
-const PasswordUtils = require("../utils/password.util.js");
-const generateToken = require("../utils/jwt.util.js");
-const { use } = require("react");
+    const prisma = require("../lib/prisma.js");
+    const PasswordUtils = require("../utils/password.util.js");
+    const generateToken = require("../utils/jwt.util.js");
 
-class AuthService {
-    static async register({ nom, prenom, email, motdeDePasse, role }) {
-        // Vérifier si l'user est déjà exister
-        const existing = await prisma.utilisateur.findUnique({ where: { email }});
-        if (existing) throw new Error("Email déjà existant");
-        
-        // Validation de la mdp
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-        if (!passwordRegex.test(motdeDePasse)) {
-            throw new Error("Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre.");
+    class AuthService {
+        static async register({ nom, prenom, email, motDePasse, role }) {
+            // Vérifier si l'user est déjà exister
+            const existing = await prisma.utilisateur.findUnique({ where: { email }});
+            if (existing) throw new Error("Email déjà existant");
+            
+            // console.log("Mot de passe reçu :", motDePasse);
+
+            // Validation de la mdp
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+            if (!passwordRegex.test(motDePasse)) {
+                throw new Error("Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.");
+            }
+
+            const hashedPassord = await PasswordUtils.hashPassword(motDePasse);
+
+            const user = await prisma.utilisateur.create({
+                data: {
+                    nom,
+                    prenom,
+                    email,
+                    motDePasse: hashedPassord,
+                    role,
+                },
+            });
+
+            return {
+                id: user.id,
+                email: user.email,
+                token: generateToken({ id: user.id, role: user.role }),
+            };
         }
 
-        const hashedPassord = await PasswordUtils.hashPassword(motdeDePasse);
+        static async login({ email, motDePasse }) {
+            const user = await prisma.utilisateur.findUnique({ where: { email } });
+            if (!user) throw new Error("Email ou mot de passe invalide");
 
-        const user = await prisma.utilisateur.create({
-            data: {
-                nom,
-                prenom,
-                email,
-                motdeDePasse: hashedPassord,
-                role,
-            },
-        });
+            const isValid = await PasswordUtils.verifyPassword(
+                motDePasse,
+                user.motDePasse,
+            );
 
-        return {
-            id: user.id,
-            email: user.email,
-            token: generateToken({ id: user.id, role: user.role }),
-        };
+            if (!isValid) throw new Error("Email ou mot de passe invalide");
+
+            return {
+                id: user.id,
+                email: user.email,
+                token: generateToken({ id: user.id, role: user.role }),
+            };
+        }
+
+        static async changePassword(id, oldPassword, newPassword) {
+            const userId = parseInt(id, 10);
+            if (isNaN(userId)) throw new Error("ID invalide");
+
+            const user = await prisma.utilisateur.findUnique({
+                where: { id: userId },
+            });
+            if (!user) throw new Error("Utilisateur non trouvé");
+
+            const isValid = await PasswordUtils.verifyPassword(
+                oldPassword,
+                user.motDePasse,
+            );
+            if(!isValid) throw new Error("Mot de passe actuel invalide");
+
+            const hashedPassord = await PasswordUtils.hashPassword(newPassword);
+
+            await prisma.utilisateur.update({
+                where: { id: userId },
+                data: { motDePasse: hashedPassord },
+            });
+
+            return { message: "Mot de passe modifié avec succès" }; 
+        }
     }
 
-    static async login({ email, motdeDePasse }) {
-        const user = await prisma.utilisateur.findUnique({ where: { email } });
-        if (!use) throw new Error("Email ou mot de passe invalide");
-
-        const isValid = await PasswordUtils.verifyPassword(
-            motdeDePasse,
-            user.motDePasse,
-        );
-
-        if (!isValid) throw new Error("Email ou mot de passe invalide");
-
-        return {
-            id: user.id,
-            email: user.email,
-            token: generateToken({ id: user.id, role: user.role }),
-        };
-    }
-
-    static async changePassword(id, oldPassword, newPassword) {
-        const userId = parseInt(id, 10);
-        if (isNaN(userId)) throw new Error("ID invalide");
-
-        const user = await prisma.utilisateur.findUnique({
-            where: { id: userId },
-        });
-        if (!user) throw new Error("Utilisateur non trouvé");
-
-        const isValid = await PasswordUtils.verifyPassword(
-            oldPassword,
-            user.motdeDePasse,
-        );
-        if(!isValid) throw new Error("Mot de passe actuel invalide");
-
-        const hashedPassord = await PasswordUtils.hashPassword(newPassword);
-
-        await prisma.utilisateur.update({
-            where: { id: userId },
-            data: { motDePasse: hashedPassord },
-        });
-
-        return { message: "Mot de passe modifié avec succès" }; 
-    }
-}
-
-module.exports = AuthService;
+    module.exports = AuthService;

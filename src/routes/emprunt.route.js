9a8bed1c132b1e5949cd992b152cf9f37df982a0
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-26 18:21:41 +0300

    feat: emprunt.route

---
+++ src/routes/emprunt.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const empruntController = require("../controllers/emprunt.controller.js");
+const auth = require("../middlewares/authMiddleware.js");
+
+router.post("/create", auth, empruntController.createEmprunt);
+router.get("/:id", auth, empruntController.getEmpruntById);
+router.get("/AllEmprunts", auth, empruntController.getAllEmprunts);
+router.put("/:id", auth, empruntController.updateEmprunt);
+router.delete("/:id", auth, empruntController.deleteEmprunt);
+
+module.exports = router;

7eb6b3acbf401c181c3c0392eaf9f40698db573b
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-26 20:18:55 +0300

    fix: emprunt.route

--- src/routes/emprunt.route.js
+++ src/routes/emprunt.route.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const empruntController = require("../controllers/emprunt.controller.js");
 const auth = require("../middlewares/authMiddleware.js");
 
-router.post("/create", empruntController.createEmprunt);
+router.post("/create",auth, empruntController.createEmprunt);
 router.get("/:id", auth, empruntController.getEmpruntById);
 router.get("/AllEmprunts", auth, empruntController.getAllEmprunts);
+router.get("/userEmprunts", auth, empruntController.getUserEmprunts);
 router.put("/:id", auth, empruntController.updateEmprunt);
+router.patch("/:id/return", auth, empruntController.returnEmprunt);
 router.delete("/:id", auth, empruntController.deleteEmprunt);
 
 module.exports = router;

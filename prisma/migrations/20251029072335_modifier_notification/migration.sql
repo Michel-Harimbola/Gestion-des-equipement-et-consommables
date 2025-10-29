2c314fb5454d9ee525e65891b1b8b2e1b4b2969f
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-10-29 10:29:08 +0300

    fix: schema.prisma: edit notification

---
+++ prisma/migrations/20251029072335_modifier_notification/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "public"."Notification_consommableId_key";
+
+-- DropIndex
+DROP INDEX "public"."Notification_empruntId_key";
---
+++ prisma/migrations/20251029072649_modifier_notification/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Notification" ALTER COLUMN "empruntId" DROP NOT NULL,
+ALTER COLUMN "consommableId" DROP NOT NULL;

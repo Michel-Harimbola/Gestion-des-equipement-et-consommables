5627c413bae4fce6bf3ca18aba7379bf96b89d44
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-08 15:59:50 +0300

    feat: demandeEmprunt.controller.js

---
+++ src/controllers/demandeEmprunt.controller.js
@@ -0,0 +1,40 @@
+const DemandeEmpruntService = require("../services/demandeEmprunt.service");
+
+exports.createDemande = async (req, res) => {
+  try {
+    const userId = req.user.id; // récupéré depuis le token
+    const { equipementId, dateRetourPrevu } = req.body;
+
+    const demande = await DemandeEmpruntService.createDemande(userId, equipementId, dateRetourPrevu);
+    res.status(201).json(demande);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getAllDemandes = async (req, res) => {
+  try {
+    const demandes = await DemandeEmpruntService.getAllDemandes();
+    res.status(200).json(demandes);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.approuverDemande = async (req, res) => {
+  try {
+    const result = await DemandeEmpruntService.approuverDemande(parseInt(req.params.id));
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.refuserDemande = async (req, res) => {
+  try {
+    const result = await DemandeEmpruntService.refuserDemande(parseInt(req.params.id));
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};

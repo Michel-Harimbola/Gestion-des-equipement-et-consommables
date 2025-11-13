1cb7f7a9ff188afe64648c91ec5074589c9ead5e
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-13 23:38:29 +0300

    feat: configuration point d'entrée du backend et socket dans app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,30 @@
 const express = require('express');
-const cors = require("cors"); 
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const notificationSocket = require("./sockets/notification.socket.js");
 
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true,       
+    },
+});
 
+app.use(cors);
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
-
+notificationSocket(io);
 
 const authRouter = require('./routes/auth.route.js');
 const userRouter = require('./routes/user.route.js');
 const empruntRouter = require("./routes/emprunt.route.js");
 const equipementRouter = require("./routes/equipement.route.js");
 const consommabletRouter = require("./routes/consommable.route.js");
+const notificationRoutes = require("./routes/notification.route.js");
 const demandeEmpruntRouter = require("./routes/demandeEmprunt.route.js");
 const utilisationConsommableRouter = require("./routes/utilisationConsommable.route.js");
 
@@ -28,6 +34,7 @@ app.use('/api/user', userRouter);
 app.use('/api/emprunt', empruntRouter);
 app.use('/api/equipement', equipementRouter);
 app.use('/api/consommable', consommabletRouter);
+app.use('./notification', notificationRoutes);
 app.use('/api/demande', demandeEmpruntRouter);
 app.use('/api/utilisation', utilisationConsommableRouter);
 

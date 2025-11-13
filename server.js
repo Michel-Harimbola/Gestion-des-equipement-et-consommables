db4a9de33640db9e2a3e1192714fe38e5346201a
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-14 01:43:58 +0300

    feat: creation serveur http et initialisation socket.io dans server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 require('dotenv').config();
-const { server } = require("./src/app.js");
+const http = require("http");
+const app = require("./src/app");
+const { init: initIO } = require("./src/socket");
+
+// Crée le serveur HTTP
+const server = http.createServer(app);
+
+// Initialise Socket.IO
+const io = initIO(server);
+
+// Event socket générique (si tu as un socket dédié, tu peux l'appeler ici)
+io.on("connection", (socket) => {
+  console.log("Un client est connecté :", socket.id);
+  // Ici tu peux écouter les events client
+});
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });

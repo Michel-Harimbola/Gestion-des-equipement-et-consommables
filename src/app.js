90bae7b3fa36c3da6c9f2d1930f7f399161ae4ab
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-14 01:41:02 +0300

    fix: supprimer des codes inutiles et simplifié les codes

--- src/app.js
+++ src/app.js
@@ -1,42 +1,20 @@
 const express = require('express');
-const http = require("http");
 const cors = require("cors");
-const { Server } = require("socket.io");
-const notificationSocket = require("./sockets/notification.socket.js");
-
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        credentials: true,       
-    },
-});
 
 app.use(cors());
 app.use(express.json());
 
-notificationSocket(io);
 
-const authRouter = require('./routes/auth.route.js');
-const userRouter = require('./routes/user.route.js');
-const empruntRouter = require("./routes/emprunt.route.js");
-const equipementRouter = require("./routes/equipement.route.js");
-const consommabletRouter = require("./routes/consommable.route.js");
-const notificationRoutes = require("./routes/notification.route.js");
-const demandeEmpruntRouter = require("./routes/demandeEmprunt.route.js");
-const utilisationConsommableRouter = require("./routes/utilisationConsommable.route.js");
-
-
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
-app.use('/api/emprunt', empruntRouter);
-app.use('/api/equipement', equipementRouter);
-app.use('/api/consommable', consommabletRouter);
-app.use('/api/notification', notificationRoutes);
-app.use('/api/demande', demandeEmpruntRouter);
-app.use('/api/utilisation', utilisationConsommableRouter);
+app.use('/api/auth', require('./routes/auth.route.js'));
+app.use('/api/user', require('./routes/user.route.js'));
+app.use('/api/emprunt', require("./routes/emprunt.route.js"));
+app.use('/api/equipement', require("./routes/equipement.route.js"));
+app.use('/api/consommable', require("./routes/consommable.route.js"));
+app.use('/api/notification', require("./routes/notification.route.js"));
+app.use('/api/demande', require("./routes/demandeEmprunt.route.js"));
+app.use('/api/utilisation', require("./routes/utilisationConsommable.route.js"));
 
 
 // Route de base(pour le test)
@@ -44,5 +22,4 @@ app.get('/', (req, res) => {
     res.send("API is running.");
 });
 
-
-module.exports = { app, server, io };
+module.exports = app;
